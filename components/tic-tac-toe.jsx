651bd63e2cedc1d5dbe328ffe071e0d5e318c73c
sammedchougule <smdchougule24@gmail.com>
2024-05-02 21:40:09 +0530

    tic-tac-toe game using reactjs

---
+++ components/tic-tac-toe.jsx
@@ -0,0 +1,35 @@
+import {useState} from 'react';
+import useTictacToe from "../hooks/use-tic-tac-toe";
+
+function TicTacToe() {
+
+  const {board, handleClick, resetGame, getStatusMessage} = useTictacToe();
+
+  return (
+    <div className='game'>
+      <div className="status">
+        {getStatusMessage()}
+        <button className='reset-button' 
+        onClick={resetGame}>
+        Reset Game
+        </button>
+      </div>
+
+      <div className="board"> {/* render by board */} 
+      {board.map((b, index) => {
+        return (
+          <button 
+        className='cell' 
+        key = {index}
+        onClick={ () => handleClick(index)}
+        disabled = {b !== null}
+        >
+          {b}
+        </button>
+        );
+      })}</div>
+    </div>
+  )
+}
+
+export default TicTacToe;
